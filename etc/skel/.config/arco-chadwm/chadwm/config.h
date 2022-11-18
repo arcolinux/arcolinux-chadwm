4247cd6a9c49ef07f9504ee905b79e98630dbf7b
arcolinuxz <arcolinuxinfo@gmail.com>
2022-11-18 17:14:02 +0100

    preview

--- etc/skel/.config/arco-chadwm/chadwm/config.h
+++ etc/skel/.config/arco-chadwm/chadwm/config.h
@@ -25,7 +25,7 @@ static const int vertpadtab         = 35;
 static const int horizpadtabi       = 15;
 static const int horizpadtabo       = 15;
 static const int scalepreview       = 4;
-static const int tag_preview        = 0;        /* 1 means enable, 0 is off */
+static const int tag_preview        = 1;        /* 1 means enable, 0 is off */
 static const int colorfultag        = 1;        /* 0 means use SchemeSel for selected non vacant tag */
 
 #define ICONSIZE 19   /* icon size */
@@ -102,6 +102,7 @@ static const Rule rules[] = {
     { "Gimp",     NULL,       NULL,       0,            0,           0,           -1 },
     { "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
     { "eww",      NULL,       NULL,       0,            0,           1,           -1 },
+    { "mintstick",      NULL,       NULL,       0,            0,           1,           -1 },
 };
 
 /* layout(s) */
