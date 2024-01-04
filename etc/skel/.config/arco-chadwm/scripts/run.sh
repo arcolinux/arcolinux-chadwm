774be5eefd466bab9e7f52a61744af90264a5333
arcolinuxz <arcolinuxinfo@gmail.com>
2024-01-04 20:40:37 +0100

    btop

--- etc/skel/.config/arco-chadwm/scripts/run.sh
+++ etc/skel/.config/arco-chadwm/scripts/run.sh
@@ -10,6 +10,9 @@ function run {
     $@&
   fi
 }
+
+#run "xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output None-1-1 --off"
+
 run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
 #run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
 #run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
